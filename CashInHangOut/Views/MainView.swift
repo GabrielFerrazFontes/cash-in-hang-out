8524134dd697beff081375f2672bde7f50696a06
Gabriel Ferraz Fontes <brielff@hotmail.com>
2025-05-19 15:15:08 -0300

    Add Tab Bar

--- CashInHangOut/Views/MainView.swift
+++ CashInHangOut/Views/MainView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         TabView {
+            HangOutsListView()
+                .tabItem {
+                    Label("History", systemImage: "clock.fill")
+                }
+            FriendsListView()
+                .tabItem {
+                    Label("Friends", systemImage: "person.3.fill")
+                }
             CreateHangView()
                 .tabItem {
-                    Label("Create", systemImage: "square.and.pencil")
+                    Label("Create", systemImage: "plus.circle.fill")
+                }
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
                 }
         }
     }
---
+++ CashInHangOut/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  CashInHangOut
+//
+//  Created by Gabriel Ferraz Fontes on 19/05/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
