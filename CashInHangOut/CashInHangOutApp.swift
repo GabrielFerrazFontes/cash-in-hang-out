2d2774bac438d71affa8d7a88b48d0ed59ed1274
Gabriel Ferraz Fontes <brielff@hotmail.com>
2025-05-19 10:41:31 -0300

    Initial Commit

---
+++ CashInHangOut/CashInHangOutApp.swift
@@ -0,0 +1,20 @@
+//
+//  CashInHangOutApp.swift
+//  CashInHangOut
+//
+//  Created by Gabriel Ferraz Fontes on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CashInHangOutApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ CashInHangOutTests/CashInHangOutTests.swift
@@ -0,0 +1,17 @@
+//
+//  CashInHangOutTests.swift
+//  CashInHangOutTests
+//
+//  Created by Gabriel Ferraz Fontes on 19/05/25.
+//
+
+import Testing
+@testable import CashInHangOut
+
+struct CashInHangOutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
